39067a3e715cc16a25827d45e710862bc7fe980b
Mati ul Rehman <mati.rehman.dev@gmail.com>
2025-04-18 14:29:30 +0500

    completed;

---
+++ Typescript/index.js
@@ -0,0 +1,6 @@
+var alpha = 20;
+var beta = 30;
+var gamma = 40;
+var delta = 50;
+var epsilon = 60;
+console.log(alpha + beta + gamma + delta + epsilon);
---
+++ Typescript/index.ts
@@ -0,0 +1,8 @@
+let message: string = "Hello, World!";
+console.log(message);
+
+// This is a simple TypeScript program that declares a variable `message` of type string and assigns it the value "Hello, World!".
+
+function logSomething(a: String, b: String): void {
+  console.log(a, b);
+}
